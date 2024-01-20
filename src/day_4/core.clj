216ed67318b8f1664f59b3efeb06b33a430ebe97
Bartev Vartanian <bartev@gmail.com>
2024-01-20 05:53:20 -0600

    Day 4 solved

--- src/day_4/core.clj
+++ src/day_4/core.clj
@@ -6,8 +6,6 @@
 
 ;; Scorecards on Island Island
 
-
-
 (defn extract-digits [x]
   (vec (re-seq #"\d+" x)))
 
@@ -15,6 +13,7 @@
   {:winners (vec winners)
    :my-nums (vec my-nums)})
 
+;; Different from `parse-card` below.
 (defn parse-cards [fname]
   (->> (read-file-lines-slurp fname)
        ;; (#(map (fn [line] (str/split line #":")) %))
@@ -25,14 +24,18 @@
                         (#(mapv extract-digits %))
                         ->lists)))))
 
-(defn common-items-card [{:keys [winners my-nums]}]
+(defn common-items-card
+  "Get the set of items that are common to both winners and my-nums"
+  [{:keys [winners my-nums]}]
   (set (clojure.set/intersection (set winners) (set my-nums))))
 
 
 (def card {:winners ["41" "48" "83" "86" "17"],
            :my-nums ["83" "86" "6" "31" "17" "9" "48" "53"]})
 
-(defn ->winnings [num-matches]
+(defn ->winnings
+  "Calculate how much you won by taking 2^num-matches if num-matches > 0."
+  [num-matches]
   (if (pos? num-matches)
     (Math/pow 2 (dec num-matches))
     0))
@@ -41,8 +44,8 @@
   (->> (parse-cards fname)
        (map common-items-card)
        (map count)
-       (mapv ->winnings)
-       (apply +)
+       (mapv ->winnings) ; Calculate winnings for each card.
+       (apply +) ; Sum up the winnings.
        ))
 
 (part1 "input-sample.txt")
@@ -56,8 +59,13 @@
 (defn common-items [lst1 lst2]
   (set (clojure.set/intersection (set lst1) (set lst2))))
 
-(defn parse-card [input-string]
-  (let [regex #"(?i)Card (\d+): (.+?) \| (.+)$"
+;; Different from `parse-cards` above.
+(defn parse-card
+  "Parse out the card index, winners and my numbers.
+  Watch out for different number of spaces after `Card`
+  between the sample and real data."
+  [input-string]
+  (let [regex #"(?i)Card\s*(\d+): (.+?) \| (.+)$"
         match (re-matches regex input-string)
         card (parse-long (second match))
         winners (extract-digits (nth match 2))
@@ -70,17 +78,38 @@
      ;; :my-nums my-nums
      :num-winners num-winners
      :num-copies 1}))
+
 (parse-card "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53")
-;; => {:card 1,
-;;     :winners ["41" "48" "83" "86" "17"],
-;;     :my-nums ["83" "86" "6" "31" "17" "9" "48" "53"],
-;;     :num-winners 4,
-;;     :num-copies 1}
+;; => {:card 1, :num-winners 4, :num-copies 1}
+(parse-card "Card   1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53")
+;; => {:card 1, :num-winners 4, :num-copies 1}
+
+
+;; How does `re-matches` work?
+;; 0: Complete string that matched.
+;; 1, 2, ...: Each matching group
+
+;; Note how only the <alpha><digits><alpha> part matched
+;; (re-seq #"[^\d]*(\d+)[^\d]*" "abc123def456")
+;; => (["abc123def" "123"] ["456" "456"])
+
+;; (let [input-string "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
+;;       regex #"(?i)Card (\d+): (.+?) \| (.+)$"
+;;       match (re-matches regex input-string)]
+;;   match)
+;; => ["Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
+;;     "1"
+;;     "41 48 83 86 17"
+;;     "83 86  6 31 17  9 48 53"]
+
+;; (re-seq #"[^\d]*(\d+)[^\d]*" "abc123def456")
+;; => (["abc123def" "123"] ["456" "456"])
 
 (defn parse-cards-idx [fname]
   (->> (read-file-lines-slurp fname)
        ;; (#(map (fn [line] (str/split line #":")) %))
-       (map parse-card)))
+       ;; (map parse-card)
+       ))
 
 (defn update-cards
   "Increment the :num-copies of the next :num-winners cards by the
@@ -100,16 +129,72 @@
          item))
      cards)))
 
-;; (def cards-sample [{:card 1, :num-winners 4, :num-copies 1}
-;;                    {:card 2, :num-winners 2, :num-copies 1}
-;;                    {:card 3, :num-winners 2, :num-copies 1}
-;;                    {:card 4, :num-winners 1, :num-copies 1}
-;;                    {:card 5, :num-winners 0, :num-copies 1}
-;;                    {:card 6, :num-winners 0, :num-copies 1}])
+(def cards-sample [{:card 1, :num-winners 4, :num-copies 1}
+                   {:card 2, :num-winners 2, :num-copies 1}
+                   {:card 3, :num-winners 2, :num-copies 1}
+                   {:card 4, :num-winners 1, :num-copies 1}
+                   {:card 5, :num-winners 0, :num-copies 1}
+                   {:card 6, :num-winners 0, :num-copies 1}])
 
-;; (update-cards cards-sample 1)
+(update-cards cards-sample 1)
+;; => ({:card 1, :num-winners 4, :num-copies 1}
+;;     {:card 2, :num-winners 2, :num-copies 2}
+;;     {:card 3, :num-winners 2, :num-copies 2}
+;;     {:card 4, :num-winners 1, :num-copies 2}
+;;     {:card 5, :num-winners 0, :num-copies 2}
+;;     {:card 6, :num-winners 0, :num-copies 1})
+
+(let [cards cards-sample
+      after-1 (update-cards cards-sample 1)
+      after-2 (update-cards after-1 2)
+      after-3 (update-cards after-2 3)
+      after-4 (update-cards after-3 4)]
+  {:after-1 after-1
+   :after-2 after-2
+   :after-3 after-3
+   :after-4 after-4})
+;; => {:after-1
+;;     ({:card 1, :num-winners 4, :num-copies 1}
+;;      {:card 2, :num-winners 2, :num-copies 2}
+;;      {:card 3, :num-winners 2, :num-copies 2}
+;;      {:card 4, :num-winners 1, :num-copies 2}
+;;      {:card 5, :num-winners 0, :num-copies 2}
+;;      {:card 6, :num-winners 0, :num-copies 1}),
+;;     :after-2
+;;     ({:card 1, :num-winners 4, :num-copies 1}
+;;      {:card 2, :num-winners 2, :num-copies 2}
+;;      {:card 3, :num-winners 2, :num-copies 4}
+;;      {:card 4, :num-winners 1, :num-copies 4}
+;;      {:card 5, :num-winners 0, :num-copies 2}
+;;      {:card 6, :num-winners 0, :num-copies 1}),
+;;     :after-3
+;;     ({:card 1, :num-winners 4, :num-copies 1}
+;;      {:card 2, :num-winners 2, :num-copies 2}
+;;      {:card 3, :num-winners 2, :num-copies 4}
+;;      {:card 4, :num-winners 1, :num-copies 8}
+;;      {:card 5, :num-winners 0, :num-copies 6}
+;;      {:card 6, :num-winners 0, :num-copies 1}),
+;;     :after-4
+;;     ({:card 1, :num-winners 4, :num-copies 1}
+;;      {:card 2, :num-winners 2, :num-copies 2}
+;;      {:card 3, :num-winners 2, :num-copies 4}
+;;      {:card 4, :num-winners 1, :num-copies 8}
+;;      {:card 5, :num-winners 0, :num-copies 14}
+;;      {:card 6, :num-winners 0, :num-copies 1})}
 
 ;; (update {:a 1} :a (fnil + 0) 3)
+;; => {:a 4}
+
+;; How `fnil` works. Replaces FIRST argument if `nil`.
+;; ((fnil + 0) nil 4)
+;; ;; => 4
+
+;; ((fnil + 0) 1 4)
+;; ;; => 5
+
+;; ((fnil + 0) 1 nil)
+;; ;; => Execution error (NullPointerException) at day-4.core/eval13794 (REPL:197).
+;; ;;    Cannot invoke "Object.getClass()" because "x" is null
 
 ;; (update {:name "jammes" :age 26} :foo + 10)
 
@@ -128,20 +213,12 @@
 (defn part2 [fname]
   (->> (parse-cards-idx fname)
        process-cards
-       ;; (mapv :num-copies)
-       ;; (apply +)
+       (mapv :num-copies)
+       (apply +)
        ))
+
 (part2 "input-sample.txt")
 ;; => 30
 
 (part2 "input.txt")
-;; => Execution error (IllegalArgumentException) at day-4.core/parse-card (REPL:62).
-;;    Expected string, got nil
-;; => Execution error (IllegalArgumentException) at day-4.core/parse-card (REPL:62).
-;;    Expected string, got nil
-;; => Execution error (IllegalArgumentException) at day-4.core/parse-card (REPL:62).
-;;    Expected string, got nil
-;; => Execution error (IllegalArgumentException) at day-4.core/parse-card (REPL:62).
-;;    Expected string, got nil
-;; => Execution error (IllegalArgumentException) at day-4.core/parse-card (REPL:62).
-;;    Expected string, got nil
+;; => 9924412
